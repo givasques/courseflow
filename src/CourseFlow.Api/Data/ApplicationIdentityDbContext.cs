21da8f5d580648c3b69bc84f99ddd4f7cb61c5c2
unknown <giovanna.vasques1101@hotmail.com>
2026-02-06 17:03:08 -0300

    feat: adding a new RefreshTokens table in the Identity database and creating a new migration

--- src/CourseFlow.Api/Data/ApplicationIdentityDbContext.cs
+++ src/CourseFlow.Api/Data/ApplicationIdentityDbContext.cs
@@ -7,6 +7,7 @@ namespace CourseFlow.Api;
 public class ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options) 
     : IdentityDbContext(options)
 {
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
@@ -20,5 +21,20 @@ public class ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDb
         builder.Entity<IdentityUserClaim<string>>().ToTable("asp_net_user_claims");
         builder.Entity<IdentityUserLogin<string>>().ToTable("asp_net_user_logins");
         builder.Entity<IdentityUserToken<string>>().ToTable("asp_net_user_tokens");
+
+        builder.Entity<RefreshToken>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            
+            entity.Property(e => e.UserId).HasMaxLength(300);
+            entity.Property(e => e.Token).HasMaxLength(1000);
+
+            entity.HasIndex(e => e.Token).IsUnique();
+
+            entity.HasOne(e => e.User)
+                .WithMany()
+                .HasForeignKey(e => e.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
     }
 }
