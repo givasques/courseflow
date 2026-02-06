617ebb4b87a97d05dcfc16e81e851abf7ba4cba4
unknown <giovanna.vasques1101@hotmail.com>
2026-02-06 12:38:24 -0300

    feat: reorganizing the Program.cs file with extension methods

---
+++ src/CourseFlow.Api/DependencyInjection.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace CourseFlow.Api;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddApiServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+    public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSnakeCaseNamingConvention();
+
+            if(builder.Environment.IsDevelopment())
+            {
+                options
+                    .UseSqlite(
+                        connectionString,
+                        sqliteOptions => sqliteOptions
+                            .MigrationsHistoryTable(HistoryRepository.DefaultTableName));
+            } 
+            else
+            {
+                options
+                    .UseNpgsql(
+                        connectionString,
+                        npgsqlOptions => npgsqlOptions
+                            .MigrationsHistoryTable(HistoryRepository.DefaultTableName));
+            }
+        });
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddObservability(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddOpenTelemetry()
+        .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+        .WithTracing(tracing =>
+        {
+            tracing
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddConsoleExporter();    
+        })
+        .WithMetrics(metrics =>
+        {
+            metrics
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddConsoleExporter();
+        });
+
+        return builder;
+    }
+}
--- src/CourseFlow.Api/Program.cs
+++ src/CourseFlow.Api/Program.cs
@@ -10,51 +10,10 @@ using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSnakeCaseNamingConvention();
-
-    if(builder.Environment.IsDevelopment())
-    {
-        options
-            .UseSqlite(
-                connectionString,
-                sqliteOptions => sqliteOptions
-                    .MigrationsHistoryTable(HistoryRepository.DefaultTableName));
-    } 
-    else
-    {
-        options
-            .UseNpgsql(
-                connectionString,
-                npgsqlOptions => npgsqlOptions
-                    .MigrationsHistoryTable(HistoryRepository.DefaultTableName));
-    }
-});
-
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
-    .WithTracing(tracing =>
-    {
-        tracing
-            .AddAspNetCoreInstrumentation()
-            .AddHttpClientInstrumentation()
-            .AddConsoleExporter();    
-    })
-    .WithMetrics(metrics =>
-    {
-        metrics
-            .AddHttpClientInstrumentation()
-            .AddAspNetCoreInstrumentation()
-            .AddConsoleExporter();
-    });
+builder
+    .AddApiServices()
+    .AddDatabase()
+    .AddObservability();
     
 var app = builder.Build();
 
